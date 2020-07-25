7b9f39961df2346abc664dc042a280440a2ee98f
Daniel Bonkowski <nicosnakecod@gmail.com>
2020-07-25 20:49:46 +0200

    Adds progress bar with UI responsiveness button check

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,14 +1,28 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.udacity.gradle.builditbigger.R;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 
 /**
@@ -16,13 +30,48 @@ import com.udacity.gradle.builditbigger.R;
  */
 public class MainActivityFragment extends Fragment {
 
+    private static ProgressBar mProgressBar;
+    private Button mJokeButton;
+    private Button mIncreaseButton;
+    private TextView mIncreaseTextView;
+    private int mCount = 0;
+    private static ButtonClickListener mButtonClickListener;
+
     public MainActivityFragment() {
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mButtonClickListener = (ButtonClickListener) context;
+    }
+
+    public interface ButtonClickListener{
+        public void callback(String result);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mProgressBar = (ProgressBar) root.findViewById(R.id.main_progress_bar);
+        final Context context = getContext();
+
+        mIncreaseTextView = root.findViewById(R.id.increase_text_view);
+        mIncreaseButton = root.findViewById(R.id.increase_button);
+
+        mIncreaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ++mCount;
+                mIncreaseTextView.setText(String.valueOf(mCount));
+            }
+        });
+
+        mJokeButton = root.findViewById(R.id.joke_button);
+        setupJokeButton();
+
 
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
@@ -33,5 +82,65 @@ public class MainActivityFragment extends Fragment {
                 .build();
         mAdView.loadAd(adRequest);
         return root;
+    }
+
+
+    public void setupJokeButton(){
+
+        final Context context = getContext();
+
+        mJokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    new EndpointsAsyncTask().execute(new Pair<Context, String>(context, "Daniel"));
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public static class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+
+        private static MyApi myApiService = null;
+
+        @Override
+        protected void onPreExecute() {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+
+
+            if(myApiService == null){
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> request) throws IOException {
+                                request.setDisableGZipContent(true);
+                            }
+                        });
+                myApiService = builder.build();
+            }
+
+            String name = params[0].second;
+
+            try{
+                return myApiService.sayHi(name).execute().getData();
+            }catch (IOException e){
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            mProgressBar.setVisibility(View.GONE);
+
+            mButtonClickListener.callback(result);
+        }
     }
 }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,40 +1,24 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
-import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
-
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-
 import gradle.udacity.displaylibrary.DisplayActivity;
-import gradle.udacity.jokelibrary.Joke;
 
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements MainActivityFragment.ButtonClickListener {
 
     private final static String EXTRAS_JOKE = "joke";
-    private String mJoke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,44 +42,10 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void tellJoke(View view) throws ExecutionException, InterruptedException {
-        String result = new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Daniel")).get();
+    @Override
+    public void callback(String result) {
         Intent intent = new Intent(this, DisplayActivity.class);
         intent.putExtra(EXTRAS_JOKE, result);
         startActivity(intent);
-    }
-
-
-
-    public static class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
-
-        private static MyApi myApiService = null;
-
-
-        @Override
-        protected String doInBackground(Pair<Context, String>... params) {
-
-            if(myApiService == null){
-                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                        new AndroidJsonFactory(), null)
-                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                            @Override
-                            public void initialize(AbstractGoogleClientRequest<?> request) throws IOException {
-                                request.setDisableGZipContent(true);
-                            }
-                        });
-                myApiService = builder.build();
-            }
-
-            String name = params[0].second;
-
-            try{
-                return myApiService.sayHi(name).execute().getData();
-            }catch (IOException e){
-                return e.getMessage();
-            }
-        }
-
     }
 }
